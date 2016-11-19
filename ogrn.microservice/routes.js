14f776f2bf83a3e74ea83490be3e7a1d89c2106f
G.Ilshat <gaifullin.ilshat@gmail.com>
2016-11-19 10:03:53 +0300

    t

--- ogrn.microservice/routes.js
+++ ogrn.microservice/routes.js
@@ -3,8 +3,16 @@
 var express = require('express');
 var router = express.Router();
 
-router.get("/getdata/:type", function (req, res) {
-    res.send('HELLO WORLD');
+router.get("/getdata/:type", function (req, res, next) {
+    if (req.params.type == 'юр') {
+        res.send('11');
+    } else if (req.params.type == 'ип') {
+        res.send('22');
+    } else {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    }
 });
 
 module.exports = router;
--- test/ogrn.spec.js
+++ test/ogrn.spec.js
@@ -5,14 +5,14 @@ var supertest = require('supertest');
 var superagent = require('superagent');
 
 //var PORT = process.env.PORT || '3000';
-var config = require('../config');
+var config = require('../ogrn.microservice/config');
 var api = supertest('http://localhost:' + config.port);
 
 
 describe('Валидация огрн', function () {
-    it('', function (done) {
-        api.get('/getdata/?p=юр').end(function (err, res) {
-            expect(res.text).toBe('HELLO WORLD');
+    it('Валидация длины огрн для юр.лиц', function (done) {
+        api.get('/getdata/юр').end(function (err, res) {
+            expect(res.text).to.have.length(13);
             done();
         });
     });
