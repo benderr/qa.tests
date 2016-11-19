00fa37ebe036e628fbc11539039c1154b5b1fd08
Eugene <alushkin1945@gmail.com>
2016-11-19 13:02:16 +0500

    'тест'

--- test/ogrn.spec.js
+++ test/ogrn.spec.js
@@ -2,7 +2,6 @@
 
 var expect = require('chai').expect;
 var supertest = require('supertest');
-var superagent = require('superagent');
 
 //var PORT = process.env.PORT || '3000';
 var config = require('../ogrn.microservice/config');
@@ -10,7 +9,7 @@ var api = supertest('http://localhost:' + config.port);
 
 
 describe('Валидация огрн', function () {
-    it('Валидация длины огрн для юр.лиц', function (done) {
+    it('Валидация огрн для юр.лиц', function (done) {
         api.get('/getdata/юр').end(function (err, res) {
             expect(res.text).to.have.length(13);
             done();
